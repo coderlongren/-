97d1e9a161aa1073f49b3eb0da40757d089a4402
Mr ren <coderlongren@gmail.com>
2016-06-10 16:38:25 +0800

    Add files via upload

---
+++ windos.cpp
@@ -0,0 +1,50 @@
+ /*#include <iostream>
+
+#include <Windows.h>
+
+using namespace std;
+
+void gotoxy(int x,int y)
+
+{
+
+COORD loc={x,y};
+
+HANDLE hOutput=GetStdHandle(STD_OUTPUT_HANDLE);
+
+SetConsoleCursorPosition(hOutput,loc);
+
+}
+
+int main()
+
+{
+
+gotoxy(50,0);
+
+cout<<"Hello World!"<<endl;
+
+system("pause");
+
+return 0;
+
+}
+  初试 windos,h   试验了一下  果然功能强大  */
+#include<stdio.h>    
+#include<windows.h>   
+int main()    
+{  
+  HANDLE hOut;  
+  COORD pos= {0, 0}; 
+  int i;  
+  hOut = GetStdHandle(STD_OUTPUT_HANDLE);   //句柄设置
+  SetConsoleTextAttribute(hOut, 0x01|0x05); //颜色设置  
+  for(i=0;i<20;i++)    
+  {  
+  pos.X=i;   
+  pos.Y=i;  
+  SetConsoleCursorPosition(hOut, pos);  //坐标定位
+  printf("%d %d:Hello World!\n",pos.X,pos.Y);    
+}  
+  return 0;   
+} 
